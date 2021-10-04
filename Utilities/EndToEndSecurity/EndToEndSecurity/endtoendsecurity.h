2935a9d558f50e6dc099d5439b2003fb3b973ac0
Seishi Nakano <Seishi@kal.com>
2021-10-04 13:34:49 +0200

    Added an end to end security in C for pasing token and validation of the token.

---
+++ Utilities/EndToEndSecurity/EndToEndSecurity/endtoendsecurity.h
@@ -0,0 +1,10 @@
+/***********************************************************************************************\
+ * (C) KAL ATM Software GmbH, 2021
+ * KAL ATM Software GmbH licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+\***********************************************************************************************/
+
+#pragma once
+
+
+extern "C" bool ValidateToken(char const* Token, size_t TokenLength);
---
+++ Utilities/EndToEndSecurity/EndToEndSecurityTest/pch.cpp
@@ -0,0 +1,9 @@
+/***********************************************************************************************\
+ * (C) KAL ATM Software GmbH, 2021
+ * KAL ATM Software GmbH licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+\***********************************************************************************************/
+
+#include "pch.h"
+
+// When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
